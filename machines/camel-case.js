b6ca2b5bf8ff7a5ec89cb05e77f2b3316d30201e
Mike McNeil <mike@balderdash.co>
2015-04-30 13:23:49 -0500

    Enhance metadata for camel-case

--- machines/camel-case.js
+++ machines/camel-case.js
@@ -1,16 +1,30 @@
 module.exports = {
-  friendlyName: 'Convert to camel case',
-  description: 'Build a new camel-cased version of the specified dash-delimited string.',
+
+
+  friendlyName: 'Convert string to camel-case',
+
+
+  description: 'Build a new camel-cased version of the specified string.',
+
+
   extendedDescription: 'Returns a version of the string with dashes removed, using medial capitalization to separate words instead. See http://en.wikipedia.org/wiki/CamelCase for more information.',
+
+
   sync: true,
+
+
   cacheable: true,
+
+
   inputs: {
     string: {
       example: 'foo-bar-baz',
-      description: 'The dash-delimited string to convert',
+      description: 'The string to convert (dash-delimited or otherwise)',
       required: true
     }
   },
+
+
   exits: {
     success: {
       description: 'OK.',
@@ -20,19 +34,26 @@ module.exports = {
       description: 'Unexpected error occurred.'
     }
   },
+
+
   defaultExit: 'success',
+
+
   fn: function (inputs, exits) {
 
-    var parts = inputs.string.split(/[\W_]/);
-    var transformedParts = [];
-    parts.forEach(function(part, index) {
-      part = part.toLowerCase();
-      if (index !== 0) {
-        part = part[0].toUpperCase()+part.substr(1);
-      }
-      transformedParts.push(part);
-    });
-    return exits.success(transformedParts.join(''));
+    var _ = require('lodash');
+    return exits.success(_.camelCase(inputs.string));
+
+    // var parts = inputs.string.split(/[\W_]/);
+    // var transformedParts = [];
+    // parts.forEach(function(part, index) {
+    //   part = part.toLowerCase();
+    //   if (index !== 0) {
+    //     part = part[0].toUpperCase()+part.substr(1);
+    //   }
+    //   transformedParts.push(part);
+    // });
+    // return exits.success(transformedParts.join(''));
 
   }
 
