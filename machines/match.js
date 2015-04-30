d57d234978110989bee4e233fd42f887d228fdda
Mike McNeil <mike@balderdash.co>
2015-04-30 13:43:12 -0500

    Improve regexp match machine with better help text and extend it to return the found index and support configurable case-sensitivity (on by default)

--- machines/match.js
+++ machines/match.js
@@ -1,54 +1,114 @@
 module.exports = {
+
+
   friendlyName: 'Search string using regexp',
+
+
   description: 'Search a string using a regular expression and return the first match.',
+
+
   sync: true,
+
+
   cacheable: true,
+
+
   inputs: {
+
     string: {
+      friendlyName: 'String to search',
       example: 'hello world',
-      description: 'The string to search',
+      description: 'The string to search (i.e. "haystack")',
       required: true
     },
+
     regexp: {
+      friendlyName: 'Regular expression',
       example: 'world',
-      description: 'The regular expression to match against (as a string- don\'t include prefix and suffix slashes)',
+      description: 'The regular expression to match against (i.e. "metal detector")',
+      extendedDescription: 'The regular expression should be specified as a string WIHOUUT including leading or trailing slashes or modifiers like /gi. It will be escaped automatically using `_.escapeRegExp()`',
       required: true
+    },
+
+    caseInsensitive: {
+      friendlyName: 'Case insensitive?',
+      description: 'Whether or not you care about uppercase/lowercase letters.',
+      extendedDescription: 'This will build the regular expression using the `/i` modifier.',
+      example: true,
+      advanced: true,
+      defaultsTo: true
     }
+
   },
+
+
   exits: {
+
     success: {
       friendlyName: 'match found',
       description: 'Returns the matched substring.',
-      example: 'world',
+      example: {
+        found: 'world',
+        at: 6
+      }
     },
+
     invalidRegexp: {
-      friendlyName: 'invalid regex',
+      friendlyName: 'invalid regexp',
       description: 'Provided regular expression is invalid (cannot be instantiated into a RegExp object)'
     },
-    fail: {
+
+    notFound: {
       friendlyName: 'no match found',
-      description: 'No match found'
+      description: 'No match found.'
     },
+
     error: {
       description: 'Unexpected error occurred.'
     }
+
   },
+
+
   defaultExit: 'success',
+
+
   fn: function (inputs, exits) {
+
+    var _ = require('lodash');
+
+    // Case-insensitive by default
+    if (_.isUndefined(inputs.caseInsensitive)) {
+      inputs.caseInsensitive = true;
+    }
 
     // Check that the regexp is valid
     var regexp;
     try {
-      regexp = new RegExp(inputs.regexp);
+      // Then escape the provided string before instantiating
+      regexp = _.escapeRegExp(inputs.regexp);
+
+      // Then construct it
+      // (and if relevant, enable case-insensitivity)
+      if (inputs.caseInsensitive) {
+        regexp = new RegExp(regexp, 'i');
+      }
+      else {
+        regexp = new RegExp(regexp);
+      }
     } catch (e) {
       return exits.invalidRegexp(e);
     }
 
     var matches = inputs.string.match(regexp);
     if (!matches) {
-      return exits.fail();
+      return exits.notFound();
     }
-    return exits.success(matches[0]);
+
+    return exits.success({
+      found: matches[0],
+      at: matches.index
+    });
 
   }
 
