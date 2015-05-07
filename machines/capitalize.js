fd4ce5e1fc18dd6abca8bd09f40774f986173453
Mike McNeil <mike@balderdash.co>
2015-05-07 07:20:46 -0500

    Add actual implementation + a couple of tests.

--- machines/capitalize.js
+++ machines/capitalize.js
@@ -37,23 +37,34 @@ module.exports = {
 
 
   exits: {
+
     success: {
       friendlyName: 'then',
       description: 'OK.',
       example: 'Villeriño',
-    },
-    error: {
-      description: 'Unexpected error occurred.'
     }
+
   },
-
-
-  defaultExit: 'success',
 
 
   fn: function (inputs, exits) {
     var _ = require('lodash');
-    return exits.success(_.capitalize(inputs.string));
+
+    // If a custom character index was NOT specified, just
+    // capitalize the string.
+    if (_.isUndefined(inputs.at)) {
+      return exits.success(
+        _.capitalize(inputs.string)
+      );
+    }
+
+    // Otherwise, do some surgery:
+    return exits.success(
+      inputs.string.slice(0, inputs.at) +
+      inputs.string.slice(inputs.at, inputs.at+1).toUpperCase() +
+      inputs.string.slice(inputs.at+1)
+    );
+
   }
 
 };
