9e7f02d72493e7e4468270cbc614f237f6f0b72e
Scott Gress <scottmgress@gmail.com>
2016-07-26 13:54:57 -0500

    "capitalize" machine: cleanup metadata and add code and tests for calling alternative exits

--- machines/capitalize.js
+++ machines/capitalize.js
@@ -1,7 +1,7 @@
 module.exports = {
 
 
-  friendlyName: 'Capitalize a string',
+  friendlyName: 'Capitalize string',
 
 
   description: 'Capitalize the first (or any) letter in a string.',
@@ -25,9 +25,9 @@ module.exports = {
     },
 
     at: {
-      friendlyName: 'Which letter?',
+      friendlyName: 'Character position',
       description: 'The index of the letter to capitalize within the string.',
-      extendedDescription: 'Strings are indexed starting from the left at 0.',
+      extendedDescription: 'Strings are indexed starting from the left at 0.  This value must be a non-negative integer.  If the given index is greater than the input string length, the input string will be returned unchanged.',
       example: 0,
       defaultsTo: 0
     }
@@ -47,20 +47,31 @@ module.exports = {
 
 
   fn: function (inputs, exits) {
+
+    // Import `lodash`.
     var _ = require('lodash');
 
     // If a custom character index was NOT specified, just
-    // capitalize the string.
+    // capitalize the string and return through the `success` exit.
     if (_.isUndefined(inputs.at)) {
       return exits.success(
         _.capitalize(inputs.string)
       );
     }
 
-    // Otherwise, do some surgery:
+    // Otherwise, if the specified index is invalid, trigger `error`.
+    if (inputs.at !== Math.floor(inputs.at) || inputs.at < 0) {
+      return exits.error(new Error('The configured value for `at` must be a non-negative integer.'));
+    }
+
+    // If the specified index is valid, do some surgery and return through
+    // the `success` exit.
     return exits.success(
+      // Get all characters up to the index
       inputs.string.slice(0, inputs.at) +
-      inputs.string.slice(inputs.at, inputs.at+1).toUpperCase() +
+      // Add the uppercased version of the character at the index
+      inputs.string.slice(inputs.at, inputs.at + 1).toUpperCase() +
+      // Add the rest of the string
       inputs.string.slice(inputs.at+1)
     );
 
